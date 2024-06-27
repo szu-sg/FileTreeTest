02a629f618f718f2518ad9cd21b9d5d5e15b99b4
王进东 <wangjindong@bytedance.com>
2024-06-27 11:18:54 +0800

    上传

---
+++ demo.py
@@ -0,0 +1,15 @@
+import os
+
+# 创建文件夹
+for i in range(1, 16):
+    folder_name = f"folder_{i}"
+    os.makedirs(folder_name)
+
+# 创建Python文件
+for i in range(1, 16):
+    file_name = f"file_{i}.py"
+    with open(file_name, "w") as file:
+        file.write("# 这是一个Python文件")
+
+print("文件夹和文件创建完成！")
+
